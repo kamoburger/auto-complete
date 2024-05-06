455d8ff37ad9b6e97ba395e4f4aec1536a108899
kamoburger <ayabonn3033@i.softbank.jp>
2024-05-06 13:10:31 +0900

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import React, { useEffect, useState } from 'react'
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function page() {
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const words = ["ホゲータ", "クワッス", "ニャオハ"]
+    const [selCom, setSelCom] = useState(0);
+    const [state, setState] = useState("");
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    useEffect(() => {
+        const handleEscape = (event) => {
+            if (event.code === "ArrowUp") {
+                setSelCom((selCom) => selCom - 1)
+            }
+            else if (event.code === "ArrowDown") {
+                setSelCom((selCom) => selCom + 1)
+            }
+            else if (event.code === "Enter") {
+                setState(words[selCom])
+            }
+        };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        document.addEventListener('keydown', handleEscape);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+        return () => {
+            document.removeEventListener('keydown', handleEscape);
+        };
+    }, [selCom]);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    if (selCom === words.length) {
+        setSelCom(0)
+    }
+
+    if (selCom === -1) {
+        setSelCom(words.length - 1)
+    }
+
+    const handleClick = (e) => {
+        console.log(e.target.value)
+    }
+
+    return (
+        <>
+            <input type="text" value={state} onChange={(event) => setState(event.target.value)} style={{backgroundColor: "", borderRadius: "3px", border: "1px solid #E8EAEE"}}/>
+            {words.map((word, index) => {
+                return (<p
+                    key={index}
+                    onClick={() => {
+                        setSelCom(index)
+                        setState(word)
+                    }}
+                    onMouseEnter={() => setSelCom(index)}
+                    style={{ backgroundColor: selCom === index ? '#F5F5F5' : 'white' }}>
+                    {word}
+                </p>)
+            })}
+            <p>{selCom}</p>
+        </>
+    )
 }
